ae5ee9d71f8cd8b323951cae7cae3f962042264d
Bartek Banachowicz <bartek.banachowicz@gmail.com>
2022-01-11 16:53:47 +0100

    Add DataHandler class
    Provide holder for matrices, file reading and writing. Fix less important issues in main-view.fxml.

--- GUIClient/src/main/java/put/poznan/guiclient/DataHandler.java
+++ GUIClient/src/main/java/put/poznan/guiclient/DataHandler.java
@@ -1,4 +1,77 @@
 package put.poznan.guiclient;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static java.lang.Math.sqrt;
+
 public class DataHandler {
+    private double[][] leftMatrix;
+    private double[][] rightMatrix;
+    private double[][] resultMatrix;
+    private int matrixSize;
+
+    public double[][] loadMatrixFromFile(Path filePath) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(String.valueOf(filePath)));
+        scanner.useDelimiter(",");
+        ArrayList<Double> buffer = new ArrayList<>();
+        double[][] matrix;
+
+        while(scanner.hasNextDouble()){
+            buffer.add(scanner.nextDouble());
+        }
+
+        if(sqrt(buffer.size())%1 == 0){
+            matrixSize = (int) sqrt(buffer.size());
+        } else{
+            //TODO - There is not square matrix at input.
+            System.out.println("It is not a square matrix.");
+        }
+
+        matrix = new double[matrixSize][matrixSize];
+
+        for(int i=0; i<matrixSize; i++){
+            for(int j=0; j<matrixSize; j++){
+                matrix[i][j] = buffer.get(0);
+                buffer.remove(0);
+            }
+        }
+        return matrix;
+    }
+
+    public void addLeftMatrix(Path filePath) throws FileNotFoundException {
+        this.leftMatrix = loadMatrixFromFile(filePath);
+    }
+
+    public void addRightMatrix(Path filePath) throws FileNotFoundException {
+        this.rightMatrix = loadMatrixFromFile(filePath);
+    }
+
+    public double[][] getLeftMatrix() {
+        return leftMatrix;
+    }
+
+    public double[][] getRightMatrix() {
+        return rightMatrix;
+    }
+
+    public void generateMatrixToFile(int size, Path filePath){
+
+    }
+
+    private void initializeResultMatrix(){
+        this.resultMatrix = new double[this.matrixSize][this.matrixSize];
+    }
+
+    public void writeToResultMatrix(int x, int y, double value){
+        this.resultMatrix[x][y] = value;
+    }
+
+    public void saveMatrixToFile(Path filePath, double[][] matrix){
+
+    }
+
 }
--- GUIClient/src/main/java/put/poznan/guiclient/GUIClient.java
+++ GUIClient/src/main/java/put/poznan/guiclient/GUIClient.java
@@ -3,6 +3,7 @@ package put.poznan.guiclient;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,7 @@ public class GUIClient extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GUIClient.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 680, 500);
+        Scene scene = new Scene(fxmlLoader.load(), 700, 480);
         stage.setTitle("Matrix multiplication");
         stage.setScene(scene);
         stage.show();
